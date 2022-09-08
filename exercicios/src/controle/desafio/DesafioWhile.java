ac96ac0aefc28bf7b33ecb7cd5848e0b88483085
Ruan Vitor <nissemonog@hotmail.com>
2022-09-08 10:58:02 -0300

    estruturas de controle

---
+++ exercicios/src/controle/desafio/DesafioWhile.java
@@ -0,0 +1,31 @@
+package controle.desafio;
+
+import java.util.Scanner;
+
+public class DesafioWhile {
+    public static void main(String[] args) {
+        Scanner entrada = Scanner(System.in);
+
+        int qtdNotas = 0;
+        double nota = 0;
+        double total = 0;
+
+        while (nota != -1) {
+            System.out.println("Informe a nota: ");
+            nota = entrada.nextDouble();
+
+            if (nota <= 10 && nota >= 0) {
+                total += nota;
+                qtdNotas++;
+            } else if (nota != -1) {
+                System.out.println("Nota inválida!!!");
+            }
+        }
+
+        //Calcular a média
+        double media = total / qtdNotas;
+        System.out.println("Média = " + media);
+
+        entrada.close();
+    }
+}
