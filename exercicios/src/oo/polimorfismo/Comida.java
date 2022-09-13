be3bb43c32abc3d087d1e89196e8248e4b6b9b60
Ruan Vitor <nissemonog@hotmail.com>
2022-09-13 09:39:11 -0300

    Seção 7: Orientação a Objetos

---
+++ exercicios/src/oo/polimorfismo/Comida.java
@@ -0,0 +1,17 @@
+package oo.polimorfismo;
+
+public abstract class Comida {
+    private double peso;
+
+    public Comida(double peso) {
+        setPeso(peso);
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+}
